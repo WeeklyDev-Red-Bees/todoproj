f90b249fbf7ebf1515114aac36321a63814d4596
Russell Hueske <hueske.russ690@gmail.com>
2016-06-23 13:53:51 -0500

    Uncomment Engine and Views configurations for server

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
