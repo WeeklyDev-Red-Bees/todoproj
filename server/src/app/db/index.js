3a41935380a738e3273a1c9e01d019b53fc649ff
Russell Hueske <hueske.russ690@gmail.com>
2016-06-30 10:48:38 -0500

    checking for errors

--- server/src/app/db/index.js
+++ server/src/app/db/index.js
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose';
 
 export function initializeDatabase() {
-  let dbURI = "mongodb://";
-  let user = process.env.DB_USER;
-  let pass = process.env.DB_PASS;
-  let host = process.env.DB_HOST;
-  let port = process.env.DB_PORT;
-  let name = process.env.DB_NAME;
-  if (user && pass) {
-    dbURI += `${user}:${pass}@`;
-  }
-  dbURI += host;
-  if (port) {
-    dbURI += ':'+port;
-  }
-  dbURI += `/${name}`;
+  let dbURI = `mongodb://${process.env.DB_URI}`;
+  // let user = process.env.DB_USER;
+  // let pass = process.env.DB_PASS;
+  // let host = process.env.DB_HOST;
+  // let port = process.env.DB_PORT;
+  // let name = process.env.DB_NAME;
+  // if (user && pass) {
+  //   dbURI += `${user}:${pass}@`;
+  // }
+  // dbURI += host;
+  // if (port) {
+  //   dbURI += ':'+port;
+  // }
+  // dbURI += `/${name}`;
   console.log(`Connecting to MongoDB with: ${dbURI}`);
   mongoose.connect(dbURI);
 }
